a8b89fe1f9f751747d27259ced1f259a9950cec9
KishoregiriH <kishvijay98@gmail.com>
2026-06-23 23:25:21 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-function saveTransaction(){
-
-let transaction = {
-    date: document.getElementById("date").value,
-    person: document.getElementById("person").value,
-    type: document.getElementById("type").value,
-    category: document.getElementById("category").value,
-    amount: document.getElementById("amount").value,
-    description: document.getElementById("description").value
-};
-
-let transactions =
-JSON.parse(localStorage.getItem("transactions")) || [];
-
-transactions.push(transaction);
-
-localStorage.setItem(
-    "transactions",
-    JSON.stringify(transactions)
-);
-
-alert("Transaction Saved Successfully");
-
-console.log(transactions);
-
 async function loadDashboard() {
 
     try {
@@ -46,14 +21,11 @@ async function loadDashboard() {
             "₹" + data.balance;
 
     }
-
     catch(error) {
 
-        console.log(error);
+        console.log("Dashboard Error:", error);
 
     }
 }
 
 loadDashboard();
-
-}
