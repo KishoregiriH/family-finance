be2cce2f2c746bccc82e150b23ad6147dce203f2
KishoregiriH <kishvijay98@gmail.com>
2026-06-23 23:20:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -23,4 +23,37 @@ alert("Transaction Saved Successfully");
 
 console.log(transactions);
 
+async function loadDashboard() {
+
+    try {
+
+        const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbyr5Uu3r7hjsz2JHsFpvwzTyMAIAlU5gSVBAm2mznw7GYIHjeQllzT-9WCHMT-ZJL0u/exec"
+        );
+
+        const data = await response.json();
+
+        document.getElementById("income").innerHTML =
+            "₹" + data.income;
+
+        document.getElementById("expense").innerHTML =
+            "₹" + data.expense;
+
+        document.getElementById("saving").innerHTML =
+            "₹" + data.saving;
+
+        document.getElementById("balance").innerHTML =
+            "₹" + data.balance;
+
+    }
+
+    catch(error) {
+
+        console.log(error);
+
+    }
+}
+
+loadDashboard();
+
 }
