09347ff4248bf45de7472c357b5b69b900ceb7e4
KishoregiriH <kishvijay98@gmail.com>
2026-06-23 23:42:22 +0530

    Fix Dashboard API

--- app.js
+++ app.js
@@ -3,29 +3,29 @@ async function loadDashboard() {
     try {
 
         const response = await fetch(
-        "https://script.google.com/macros/library/d/1MAlq3yyAFkKDwuLxI0yKcTZW_2dKmQQpk8FCJprzngn8V97B3qFyNawl/3"
+        "https://script.google.com/macros/s/AKfycbyr5Uu3r7hjsz2JHsFpvwzTyMAIAlU5gSVBAm2mznw7GYIHjeQllzT-9WCHMT-ZJL0u/exec"
         );
 
         const data = await response.json();
 
-        document.getElementById("income").innerHTML =
+        document.getElementById("income").innerText =
             "₹" + data.income;
 
-        document.getElementById("expense").innerHTML =
+        document.getElementById("expense").innerText =
             "₹" + data.expense;
 
-        document.getElementById("saving").innerHTML =
+        document.getElementById("saving").innerText =
             "₹" + data.saving;
 
-        document.getElementById("balance").innerHTML =
+        document.getElementById("balance").innerText =
             "₹" + data.balance;
 
     }
     catch(error) {
 
-        console.log("Dashboard Error:", error);
+        console.error(error);
 
     }
 }
 
-loadDashboard();
+window.onload = loadDashboard;
