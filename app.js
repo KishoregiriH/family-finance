c60ede45c36f66c04d341039c31f1a9a845d446b
KishoregiriH <kishvijay98@gmail.com>
2026-06-23 21:33:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log("Family Finance Started");
+
+function saveTransaction(){
+
+let date = document.getElementById("date").value;
+let person = document.getElementById("person").value;
+let type = document.getElementById("type").value;
+let category = document.getElementById("category").value;
+let amount = document.getElementById("amount").value;
+let description = document.getElementById("description").value;
+
+console.log({
+    date,
+    person,
+    type,
+    category,
+    amount,
+    description
+});
+
+alert("Transaction Saved");
+}
